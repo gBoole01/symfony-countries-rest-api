fb71fced49db4749d51ef5e7c91022d6bbb884df
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-01-12 21:23:25 +0100

    feat: declare Country as an ApiResource

--- src/Entity/Country.php
+++ src/Entity/Country.php
@@ -2,9 +2,11 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\CountryRepository;
 use Doctrine\ORM\Mapping as ORM;
 
+#[ApiResource]
 #[ORM\Entity(repositoryClass: CountryRepository::class)]
 class Country
 {

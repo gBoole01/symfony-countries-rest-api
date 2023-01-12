7deb273f59ff84ec386d3d38ba3c684cbbbe426e
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-01-12 21:01:31 +0100

    feat: add Country migration

---
+++ migrations/Version20230112200045.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230112200045 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE country_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE country (id INT NOT NULL, code VARCHAR(2) NOT NULL, name VARCHAR(255) NOT NULL, native VARCHAR(255) NOT NULL, phone VARCHAR(10) NOT NULL, continent VARCHAR(255) NOT NULL, capital VARCHAR(255) NOT NULL, currency VARCHAR(10) NOT NULL, emoji VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE country_id_seq CASCADE');
+        $this->addSql('DROP TABLE country');
+    }
+}

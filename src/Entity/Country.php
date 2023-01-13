b3b49bf2cb9d953f9cc6139ed13ac20cfec5ccbc
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-01-13 21:54:00 +0100

    feat: hide db identifier field and declare ISO code as unique identifier

--- src/Entity/Country.php
+++ src/Entity/Country.php
@@ -2,45 +2,84 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use App\Repository\CountryRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ApiResource(operations: [
-new Get(),
-new GetCollection()
-])]
+new GetCollection(),
+new Get()
+],
+normalizationContext: ["groups" => ["countries:read"]],
+)]
 #[ORM\Entity(repositoryClass: CountryRepository::class)]
+#[UniqueEntity('code')]
 class Country
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[ApiProperty(identifier: false)]
     private ?int $id = null;
 
+    /**
+     * ISO Code of the country
+     */
+    #[Groups("countries:read")]
     #[ORM\Column(length: 255)]
+    #[ApiProperty(identifier: true)]
     private ?string $code = null;
 
+    /**
+     * Name of the country
+     */
+    #[Groups("countries:read")]
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
+    /**
+     * Country name, as pronouced by its native speakers
+     */
+    #[Groups("countries:read")]
     #[ORM\Column(length: 255)]
     private ?string $native = null;
 
+    /**
+     * Phone international code
+     */
+    #[Groups("countries:read")]
     #[ORM\Column(length: 255)]
     private ?string $phone = null;
 
+    /**
+     * Continent where it belongs
+     */
+    #[Groups("countries:read")]
     #[ORM\Column(length: 255)]
     private ?string $continent = null;
 
+    /**
+     * Capital of the country
+     */
+    #[Groups("countries:read")]
     #[ORM\Column(length: 255)]
     private ?string $capital = null;
 
+    /**
+     * Currency in use in the country
+     */
+    #[Groups("countries:read")]
     #[ORM\Column(length: 255)]
     private ?string $currency = null;
 
+    /**
+     * Emoji representation of the flag
+     */
+    #[Groups("countries:read")]
     #[ORM\Column(length: 255)]
     private ?string $emoji = null;
 
